7cc4d4e8d8e1befc07141fa72dab5c59000103e7
R2-D1-coder <839364051@qq.com>
2022-09-01 16:01:04 +0800

    原始代码

--- src/test.cpp
+++ src/test.cpp
@@ -1,7 +1,6 @@
 #include "Mpro.h"
 #include <thread>
 #include <iostream>
-#include <ctime>
 #include <chrono>
 #include <fstream>
 #include <iostream>
@@ -9,97 +8,76 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	ofstream ofs;
-	ofs.open("route_info.csv", ios::out);
+    ofstream ofs;
+    ofs.open("route_info.csv", ios::out);
 
-	Mpro app;
-	app.Init();
-	app.Start();
-	bool isExit = false;
-	std::thread thr([&] {
-		std::list<HPP::RigidInfo> lis;
-		bool first_time = false;
-		std::chrono::steady_clock::time_point start;
-		long long stamp_start = 0;
+    Mpro app;
+    app.Init();
+    app.Start();
+    bool isExit = false;
+    std::thread thr([&] {
+        std::list<HPP::RigidInfo> lis;
+        while (!isExit)
+        {
+            app.GetResult(lis);
+            if (lis.empty())
+            {
+                continue;
+            }
 
-		while (!isExit)
-		{
-			app.GetResult(lis);
-			if (lis.empty())
-			{
-				continue;
-			}
+            for (auto iter = lis.begin(); iter != lis.end(); ++iter)
+            {
+                ofs << iter->id << ", ";
 
-			for (auto iter = lis.begin(); iter != lis.end(); ++iter)
-			{
-				ofs << iter->id << ", ";
+                ofs << iter->pose.quaternion[0] << ", ";
+                ofs << iter->pose.quaternion[1] << ", ";
+                ofs << iter->pose.quaternion[2] << ", ";
+                ofs << iter->pose.quaternion[3] << ", ";
+                ofs << iter->pose.translation[0] << ", ";
+                ofs << iter->pose.translation[1] << ", ";
+                ofs << iter->pose.translation[2] << ", ";
 
-				ofs << iter->pose.quaternion[0] << ", ";
-				ofs << iter->pose.quaternion[1] << ", ";
-				ofs << iter->pose.quaternion[2] << ", ";
-				ofs << iter->pose.quaternion[3] << ", ";
-				ofs << iter->pose.translation[0] << ", ";
-				ofs << iter->pose.translation[1] << ", ";
-				ofs << iter->pose.translation[2] << ", ";
+                ofs << iter->euler[0] << ", ";
+                ofs << iter->euler[1] << ", ";
+                ofs << iter->euler[2] << ", ";
 
-				ofs << iter->euler[0] << ", ";
-				ofs << iter->euler[1] << ", ";
-				ofs << iter->euler[2] << ", ";
+                ofs << iter->vel[0] << ", ";
+                ofs << iter->vel[1] << ", ";
+                ofs << iter->vel[2] << ", ";
 
-				ofs << iter->vel[0] << ", ";
-				ofs << iter->vel[1] << ", ";
-				ofs << iter->vel[2] << ", ";
+                ofs << iter->angular_vel[0] << ", ";
+                ofs << iter->angular_vel[1] << ", ";
+                ofs << iter->angular_vel[2] << ", ";
 
-				ofs << iter->angular_vel[0] << ", ";
-				ofs << iter->angular_vel[1] << ", ";
-				ofs << iter->angular_vel[2] << ", ";
+                ofs << iter->time_stamp << ", ";
 
-				ofs << iter->time_stamp << ", ";
+                ofs << iter->states << std::endl;
 
-				if (first_time == false)
-				{
-					start = std::chrono::high_resolution_clock::now();
-					stamp_start = iter->time_stamp;
-				}
-				auto now = start + std::chrono::nanoseconds(iter->time_stamp - stamp_start);
-				auto now_log=now.time_since_epoch();
-				ofs << (std::chrono::duration_cast<std::chrono::hours>(now_log)).count()<<":"
-					<< (std::chrono::duration_cast<std::chrono::minutes>(now_log)).count() << ":"
-					<< (std::chrono::duration_cast<std::chrono::seconds>(now_log)).count() << "."
-					<< (std::chrono::duration_cast<std::chrono::milliseconds>(now_log)).count() << "'"
-					<< (std::chrono::duration_cast<std::chrono::microseconds>(now_log)).count() << "''"
-					<< (std::chrono::duration_cast<std::chrono::nanoseconds>(now_log)).count() 
-					<< ",";
+                printf("pose: %.5f, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f\n",
+                    iter->pose.quaternion[0],
+                    iter->pose.quaternion[1],
+                    iter->pose.quaternion[2],
+                    iter->pose.quaternion[3],
+                    iter->pose.translation[0],
+                    iter->pose.translation[1],
+                    iter->pose.translation[2]
+                );
+            }
+            //std::this_thread::sleep_for(std::chrono::microseconds(1));
+            //printf("list size:%zd\n", lis.size());
+        }
+        ofs.close();
 
-				ofs << iter->states << std::endl;
+        });
+    std::thread thr1([&] {
+        getchar();
+        isExit = true;
+        });
 
-				printf("pose: %.5f, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f\n",
-					iter->pose.quaternion[0],
-					iter->pose.quaternion[1],
-					iter->pose.quaternion[2],
-					iter->pose.quaternion[3],
-					iter->pose.translation[0],
-					iter->pose.translation[1],
-					iter->pose.translation[2]
-				);
-			}
-			//std::this_thread::sleep_for(std::chrono::microseconds(1));
-			//printf("list size:%zd\n", lis.size());
-		}
-		ofs.close();
+    printf("wait............\n");
+    thr.join();
+    thr1.join();
+    app.Deinit();
 
-		});
-	std::thread thr1([&] {
-		getchar();
-		isExit = true;
-		});
-
-	printf("wait............\n");
-	thr.join();
-	thr1.join();
-	app.Deinit();
-
-	return 0;
+    return 0;
 }
-
-
