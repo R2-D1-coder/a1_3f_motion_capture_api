6e45370d5fbe441492405c5e9dc06823773e5885
R2-D1-coder <839364051@qq.com>
2022-09-05 16:01:04 +0800

    稳定初版

--- src/test.cpp
+++ src/test.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(int argc, char** argv)
@@ -17,7 +18,7 @@ int main(int argc, char** argv)
 	time(&rawtime);
 	info = localtime(&rawtime);
 	strftime(buffer, 80, "route_%m_%d_%H_%M_%S.csv", info);
-	ofs.open( buffer, ios::out);
+	ofs.open(buffer, ios::out);
 
 	Mpro app;
 	app.Init();
@@ -42,29 +43,6 @@ int main(int argc, char** argv)
 
 				for (auto iter = lis.begin(); iter != lis.end(); ++iter)
 				{
-					ofs << iter->id << ", ";
-
-					ofs << iter->pose.quaternion[0] << ", ";
-					ofs << iter->pose.quaternion[1] << ", ";
-					ofs << iter->pose.quaternion[2] << ", ";
-					ofs << iter->pose.quaternion[3] << ", ";
-					ofs << iter->pose.translation[0] << ", ";
-					ofs << iter->pose.translation[1] << ", ";
-					ofs << iter->pose.translation[2] << ", ";
-
-					ofs << iter->euler[0] << ", ";
-					ofs << iter->euler[1] << ", ";
-					ofs << iter->euler[2] << ", ";
-
-					ofs << iter->vel[0] << ", ";
-					ofs << iter->vel[1] << ", ";
-					ofs << iter->vel[2] << ", ";
-
-					ofs << iter->angular_vel[0] << ", ";
-					ofs << iter->angular_vel[1] << ", ";
-					ofs << iter->angular_vel[2] << ", ";
-
-					ofs << iter->time_stamp << ", ";
 
 					if (first_time == false)
 					{
@@ -72,7 +50,7 @@ int main(int argc, char** argv)
 						start = std::chrono::system_clock::now();
 						stamp_start = iter->time_stamp;
 					}
-					auto now = start + std::chrono::nanoseconds(iter->time_stamp - stamp_start);
+					auto now = start + std::chrono::microseconds(iter->time_stamp - stamp_start);
 					auto now_log = now.time_since_epoch();
 					typedef std::chrono::duration<int, std::ratio_multiply<std::chrono::hours::period, std::ratio<8>>::type> Days; /* UTC: +8:00 */
 					Days days = std::chrono::duration_cast<Days>(now_log);
@@ -92,23 +70,29 @@ int main(int argc, char** argv)
 					ofs << hours.count() + 8 << ":";
 					ofs.fill('0');					ofs.width(2);
 					ofs << minutes.count() << ":";
+					std::cout << minutes.count() << ":";
 					ofs.fill('0');					ofs.width(2);
 					ofs << seconds.count() << ".";
+					std::cout << seconds.count() << ".";
 					ofs.fill('0');					ofs.width(3);
 					ofs << milliseconds.count() << "'";
+					std::cout << milliseconds.count() << "'";
 					ofs.fill('0');					ofs.width(3);
 					ofs << microseconds.count() << "''";
-					ofs.fill('0');					ofs.width(3);
-					ofs << nanoseconds.count() << "," << std::endl;
+					std::cout << microseconds.count() << "''";
+					ofs.fill('0');					ofs.width(1);
+					//ofs << nanoseconds.count() << "," << std::endl;
+					//std::cout << nanoseconds.count() << "," << std::endl;
+					ofs << 0 << "," ;
+					std::cout << 0 << std::endl;
 
-					printf("pose: %.5f, %.5f, %.5f, %.5f, %.5f, %.5f, %.5f\n",
-						iter->pose.quaternion[0],
-						iter->pose.quaternion[1],
-						iter->pose.quaternion[2],
-						iter->pose.quaternion[3],
-						iter->pose.translation[0],
-						iter->pose.translation[1],
-						iter->pose.translation[2]);
+					ofs << iter->vel[0] << ", ";
+					ofs << iter->vel[1] << ", ";
+					ofs << iter->vel[2] << ", ";
+
+					ofs << iter->angular_vel[0] << ", ";
+					ofs << iter->angular_vel[1] << ", ";
+					ofs << iter->angular_vel[2] << ", " << std::endl;
 				}
 				// std::this_thread::sleep_for(std::chrono::microseconds(1));
 				// printf("list size:%zd\n", lis.size());
